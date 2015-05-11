1bf346b5dd41a0bd727cf878894efce4739c48c0
GoofyHTS <goofyhts@gmail.com>
2015-05-11 20:41:55 +0200

    initial project creation

---
+++ DotNetTraktV2/MyClass.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Goofy
+ * Date: 5/11/2015
+ * Time: 8:37 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+
+namespace DotNetTraktV2
+{
+	/// <summary>
+	/// Description of MyClass.
+	/// </summary>
+	public class MyClass
+	{
+		
+	}
+}
